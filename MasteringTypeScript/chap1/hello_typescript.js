e95917177a6b86a588edd0df948d27e10732039e
owari-taro <taro.biwajima@gmail.com>
2021-12-08 16:43:36 +0900

    my first typescript

---
+++ MasteringTypeScript/chap1/hello_typescript.js
@@ -0,0 +1,12 @@
+/**
+ *this is a test
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {*}  {number}
+ */
+function calculate(a, b, c) {
+    return (a * b) + c;
+}
+console.log(calculate(3, 4, 5));
---
+++ MasteringTypeScript/chap1/hello_typescript.ts
@@ -0,0 +1,15 @@
+/**
+ *this is a test
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {*}  {number}
+ */
+function calculate(a: number, b: number, c: number): number {
+    
+    return (a * b) + c;
+}
+
+
+console.log(calculate(3,4,5));
